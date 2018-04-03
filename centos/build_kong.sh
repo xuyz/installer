ca589a1ab8ad53367693fcb94b7ba1074cd7b2a3
xuyz <xuyz1988@163.com>
2018-04-03 15:32:06 +0800

    update build kong

--- centos/build_kong.sh
+++ centos/build_kong.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-yum -y install yum-utils --downloadonly --downloaddir=utils
-yum -y install epel-release --downloadonly --downloaddir=utils
-
 ### download depends
+# yum-utils
+yum -y install yum-utils --downloadonly --downloaddir=utils
+
+# epel
+yum -y install epel-release --downloadonly --downloaddir=epel
+
 # openresty
+yum -y install yum-utils
 yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
-yum -y install openresty openresty-resty --downloadonly --downloaddir=openresty
+yum -y install openresty --downloadonly --downloaddir=openresty
 
 # kong
-
+wget https://bintray.com/kong/kong-community-edition-rpm/rpm -O /etc/yum.repos.d/bintray-kong-kong-community-edition-rpm.repo
+yum install epel-release --downloadonly --downloaddir=kong
+yum -y install kong-community-edition --downloadonly --downloaddir=kong
 
 # postgres
 yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
--- centos/resthub_release.sh
+++ centos/resthub_release.sh
@@ -5,31 +5,22 @@ CURDIR=`pwd`
 # yum-utils
 cd $CURDIR
 cd utils
-yum -y install utils/epel-release-7-11.noarch.rpm
-
+yum -y install *.rpm
 
 # epel
 cd $CURDIR
 cd epel
-yum -y install epel-release-7-9.noarch.rpm
+yum -y install *.rpm
 
 # openresty
 cd $CURDIR
 cd openresty
-yum -y install openresty-pcre-8.41-1.el7.centos.x86_64.rpm
-yum -y install openresty-openssl-1.0.2k-1.el7.centos.x86_64.rpm
-yum -y install openresty-zlib-1.2.11-3.el7.centos.x86_64.rpm
-yum -y install openresty-resty-1.13.6.1-1.el7.centos.noarch.rpm
-yum -y install openresty-1.13.6.1-1.el7.centos.x86_64.rpm
+yum -y install *.rpm
 
 # postgresql
 cd $CURDIR
-yum -y install glib2-2.50.3-3.el7.x86_64.rpm
-yum -y install initscripts-9.49.39-1.el7_4.1.x86_64.rpm
-yum -y install kmod-20-15.el7_4.7.x86_64.rpm
-yum -y install dracut-network-033-502.el7_4.1.x86_64.rpm dracut-033-502.el7_4.1.x86_64.rpm dracut-config-rescue-033-502.el7_4.1.x86_64.rpm
-yum -y install systemd-219-42.el7_4.10.x86_64.rpm  systemd-libs-219-42.el7_4.10.x86_64.rpm systemd-sysv-219-42.el7_4.10.x86_64.rpm libgudev1-219-42.el7_4.10.x86_64.rpm
-yum -y install postgresql96-libs-9.6.8-1PGDG.rhel7.x86_64.rpm postgresql96-9.6.8-1PGDG.rhel7.x86_64.rpm postgresql96-server-9.6.8-1PGDG.rhel7.x86_64.rpm
+cd postgresql
+yum -y install *.rpm
 
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
 systemctl enable postgresql-9.6
@@ -43,5 +34,13 @@ sudo -u postgres createdb -O kong kong_tests
 
 # kong
 cd $CURDIR
+cd kong
+yum -y install *.rpm
 
-#
+sed -i ~/.bash_profile -e "/export PATH/d"
+echo "PATH=$PATH:/usr/local/openresty/bin/:/usr/local/openresty/nginx/sbin" >> ~/.bash_profile
+echo "export PATH" >> ~/.bash_profile
+echo "ulimit -n 8192" >>  ~/.bash_profile
+source ~/.bash_profile
+
+
