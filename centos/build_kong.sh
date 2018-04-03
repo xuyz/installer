128f82adb32cfdcaf4502556a79efb25d260921d
xuyz <xuyz1988@163.com>
2018-04-03 13:33:20 +0800

    add build_kong

---
+++ centos/build_kong.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+### download depends
+# openresty
+yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
+yum -y install openresty openresty-resty --downloadonly --downloaddir=openresty
+
+# kong
+
+
+# postgres
+yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+yum install -y postgresql96 --downloadonly --downloaddir=postgresql
+yum install -y postgresql96-server --downloadonly --downloaddir=postgresql
